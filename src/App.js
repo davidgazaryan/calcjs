8c87255701df6958f2d9254c002c90cc33af066d
davidgazaryan <126544535+davidgazaryan@users.noreply.github.com>
2023-12-22 00:19:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function reducer(state,{type,payload}){ //Add factorials and other stuff when done to make it more complicated 
+    switch(type){
+      case 'append':
+        if (payload === '.' && state.currentOperand.includes(payload)) return state 
+          return {
+          ...state,
+          currentOperand: `${state.currentOperand}${payload}`
+          }
+      case 'deletion':
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0,state.currentOperand.length - 1),
+          
+        }
+      case 'clear':
+        return {
+          ...state,
+          currentOperand: ""
+        }
+      case 'operand':
+        return {
+          ...state, // Need our spread operator so previos state doesn't get overrided by one variable.
+          previousOperand: state.currentOperand,
+          currentOperand: "",
+          operation: payload
+        }
+      case 'evaluate':
+        return {
+          ...state,
+          currentOperand: evaluate(state).toString(),
+          previousOperand: null,
+          operation: null
+
+        }
+      case 'factorial':
+        return {
+          ...state,
+          previousOperand:state.currentOperand,
+          currentOperand: `${state.currentOperand}${payload}`,
+          operation: payload
+        }
+      default:
+        throw new Error('unknown action type:' `${type}`) 
+    }
+  }
+
+  const [{currentOperand, previousOperand, operation} , dispatch] = useReducer(reducer,{currentOperand:""})
+  
   return (
     <div className="App">
       <header className="App-header">
